3cca0e467bc686e39c561f5731895a8f391ff7af
unknown <shoon2430@gmail.com>
2020-05-07 17:05:06 +0900

    delete kakao login

--- users/views.py
+++ users/views.py
@@ -71,25 +71,3 @@ class userUpdateView(LoginRequiredMixin, UpdateView):
             return next_arg
         else:
             return reverse_lazy("user:info", args=(self.request.user.pk,))
-
-
-def kakago_get_token(request):
-    app_key = os.environ.get("KAKAGO_REST_API_KEY")
-    redirect_uri = "http://127.0.0.1:8000/users/login/kakao/callback"
-    URL = f"https://kauth.kakao.com/oauth/authorize?client_id={app_key}&redirect_uri={redirect_uri}&response_type=code"
-
-    return redirect(URL)
-
-
-class KakaoException(Exception):
-    pass
-
-
-def kakaoLogin(request):
-    try:
-        code = request.GET.get("code")
-        token_request = requests.get(
-            f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id="
-        )
-    except KakaoException:
-        return redirect(reverse("users:login"))
